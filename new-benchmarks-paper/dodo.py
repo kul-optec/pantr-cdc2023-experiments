0d56cf382aabfe3eff6a968e31a8bc64b1b30f20
Pieter Pas <pieter.pas@outlook.com>
2023-05-14 00:59:50 +0200

    Update new-benchmarks-paper

--- new-benchmarks-paper/dodo.py
+++ new-benchmarks-paper/dodo.py
@@ -7,13 +7,14 @@ TOL = 1e-8
 OUTPUT = "output"
 N = 60
 NUM_SIM = 60
+PROBLEM = "quadcopter"
 
 OPT_ALM = [
-    "alm.max_iter=1000",
+    "alm.max_iter=300",
     "alm.max_time=30s",
     "alm.initial_penalty=1e4",
     "alm.penalty_update_factor=5",
-    "alm.initial_tolerance=1e-1",
+    "alm.initial_tolerance=1e2",
     "alm.tolerance_update_factor=0.1",
     "solver.stop_crit=ProjGradUnitNorm",
     "solver.max_iter=250",
@@ -25,11 +26,12 @@ SOLVER_OPT = {
     "pantr": OPT_ALM + [
         "solver.radius_factor_rejected=0.35",
         "solver.radius_factor_acceptable=0.99",
-        "solver.radius_factor_good=2.5",
-        "solver.ratio_threshold_acceptable=0.2",
-        "solver.ratio_threshold_good=0.8",
+        "solver.radius_factor_good=10",
+        "solver.ratio_threshold_acceptable=0.3",
+        "solver.ratio_threshold_good=0.5",
         "solver.max_iter=250",
-        "dir.hessian_vec_factor=0",
+        "dir.hessian_vec_factor=1",
+        "accel.tol_scale=1e-2",
     ],
     "panoc": OPT_ALM + [
         "accel.memory=50",
@@ -52,24 +54,32 @@ SOLVER_NAMES = {
     "strucpanoc": "Struc. PANOC$^+$ (50)",
     "ipopt": "Ipopt",
 }
+SOLVER_CMDS = {
+    "pantr": "pantr",
+    "panoc": "panoc",
+    "strucpanoc": "strucpanoc",
+    "ipopt": "ipopt",
+}
 
 
-def task_quadcopter():
-    problem = "quadcopter"
+def task_mpc_benchmark():
     for warm in (True, False):
         for horizon in range(Δ, N+1, Δ):
             for solver, opt in SOLVER_OPT.items():
                 if solver == "ipopt" and warm:
                     opt = opt + ["solver.warm_start_init_point=yes"]
                 formulation = "ss2" if solver == "ipopt" else "ss2p"
-                outfile = problem_name(problem, warm, solver, horizon)
+                outfile = problem_name(PROBLEM, warm, solver, horizon)
+                problem_cmd = PROBLEM
+                if PROBLEM == "realistic_quadcopter" and solver == "ipopt":
+                    problem_cmd = "dl:" + problem_cmd
                 cmd = [
                     "zsh",
                     "run-single-mpc-exp.sh",
                     OUTPUT,
                     outfile,
-                    solver,
-                    problem,
+                    SOLVER_CMDS[solver],
+                    problem_cmd,
                     formulation,
                     str(NUM_SIM),
                     str(horizon),
@@ -98,17 +108,16 @@ def plot_name(problem, warm, solver):
 
 
 def task_perfplot():
-    problem = "quadcopter"
     for warm in (True, False):
         deps = []
         results = []
         for solver in SOLVER_OPT:
             for horizon in range(Δ, N+1, Δ):
-                outfile = problem_name(problem, warm, solver, horizon)
+                outfile = problem_name(PROBLEM, warm, solver, horizon)
                 deps += [osp.join(OUTPUT, outfile + ".py")]
-            outfile = problem_name(problem, warm, solver, horizon='{}')
+            outfile = problem_name(PROBLEM, warm, solver, horizon='{}')
             results += [f"{SOLVER_NAMES[solver]}:{outfile}"]
-        name = plot_name(problem, warm, solver)
+        name = plot_name(PROBLEM, warm, solver)
         cmd = [
             "python3",
             "perfplot.py",
@@ -125,18 +134,17 @@ def task_perfplot():
         }
 
 def task_perfplot_cold_warm():
-    problem = "quadcopter"
     deps = []
     results = []
     for warm in (True, False):
         warm_str = "warm" if warm else "cold"
         for solver in SOLVER_OPT:
             for horizon in range(Δ, N+1, Δ):
-                outfile = problem_name(problem, warm, solver, horizon)
+                outfile = problem_name(PROBLEM, warm, solver, horizon)
                 deps += [osp.join(OUTPUT, outfile + ".py")]
-            outfile = problem_name(problem, warm, solver, horizon='{}')
+            outfile = problem_name(PROBLEM, warm, solver, horizon='{}')
             results += [f"{warm_str}:{SOLVER_NAMES[solver]}:{outfile}"]
-    name = f"mpc-{problem}-{NUM_SIM}-{solver}"
+    name = f"mpc-{PROBLEM}-{NUM_SIM}-{solver}"
     cmd = [
         "python3",
         "perfplot-cold-warm.py",
@@ -151,29 +159,3 @@ def task_perfplot_cold_warm():
         "targets": [osp.join(OUTPUT, name + "-avg-runtimes-quantiles-cold-warm.pdf")],
         "actions": [cmd + results],
     }
-
-
-# def task_trajplot():
-#     problem = "quadcopter"
-#     deps = []
-#     results = []
-#     warm = True
-#     warm_str = "warm" if warm else "cold"
-#     solver = "pantr"
-#         name = f"{warm_str}-{solver}"
-#         outfile = f"mpc-Δ{Δ}-{problem}-{name}"
-#         deps += [osp.join(OUTPUT, outfile + "_ids.txt")]
-#         results += [outfile]
-#     name = f"mpc-Δ{Δ}-{problem}-{warm_str}"
-#     cmd = [
-#         "python3",
-#         "../benchmarks-paper/visualize_mpc_solution.py",
-#         OUTPUT,
-#         name,
-#     ]
-#     yield {
-#         "name": name,
-#         "file_dep": deps,
-#         "targets": [osp.join(OUTPUT, name + "-solution.pdf")],
-#         "actions": [cmd + results],
-#     }
